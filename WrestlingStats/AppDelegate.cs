0715f59eb2486c78ecbb1117b96b6e7e97d7c997
eiu165 <eiu165@gmail.com>
2013-03-14 20:04:08 -0500

    rename main controller

--- WrestlingStats/AppDelegate.cs
+++ WrestlingStats/AppDelegate.cs
@@ -17,7 +17,7 @@ namespace WrestlingStats
         public override bool FinishedLaunching (UIApplication app, NSDictionary options)
         {     
             window = new UIWindow (UIScreen.MainScreen.Bounds);          
-            vc = new WrestlingStatsController ();
+            vc = new MainController ();
             window.RootViewController = vc;
             window.MakeKeyAndVisible ();
             
